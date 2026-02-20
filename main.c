d51b56743c574d2b07895bde9591d46255724ecc
Elkan Choo <echoo@42mail.sutd.edu.sg>
2026-02-20 15:08:29 +0800

    just learning how readline works

---
+++ main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Elkan Choo <echoo@42mail.sutd.edu.sg>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/20 14:13:47 by Elkan Choo        #+#    #+#             */
+/*   Updated: 2026/02/20 15:06:15 by Elkan Choo       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	main(void)
+{
+	char	*input;
+
+	while (1)
+	{
+		input = readline("echoo: ");
+		printf("input: %s\n", input);
+		free (input);
+	}
+}
